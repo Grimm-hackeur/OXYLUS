0abc88dfc22e34ec916571bb573de9735fa8ba08
ZEPHYR_DEV <klivensjeanjacques@gmail.com>
2026-03-23 18:54:30 -0500

    Update CORS origin to new backend URL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://vybee.onrender.com",
+    origin: "https://backend-oxylus.onrender.com",
     credentials: true,
   })
 );
